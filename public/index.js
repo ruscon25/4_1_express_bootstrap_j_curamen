4645e6d3ebc665ec0529c62033229311e40f29a0
Judd Ruscon T. Curamen <ruscon2513@gmail.com>
2023-10-19 20:29:20 +0800

    Added some changes to the navbar behavior

--- public/index.js
+++ public/index.js
@@ -1,1 +1,10 @@
-console.log("Yap Login UI with Register Modal");
+console.log("A sample of NavBar with Register Modal");
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarTitle = document.querySelector(".navbar-title");
+    const navbarCollapse = document.querySelector(".collapse");
+
+    navbarTitle.addEventListener("click", function() {
+        navbarCollapse.classList.toggle("show");
+    });
+});
