c94863c78e738786ebf47911223a1499bcc7ce30
Judd Ruscon T. Curamen <ruscon2513@gmail.com>
2023-10-24 21:11:20 +0800

    Optimized the code for index.html; Reduced the number of codes in index.js; Added products.html

--- public/index.js
+++ public/index.js
@@ -1,10 +1,1 @@
 console.log("A sample of NavBar with Register Modal");
-
-document.addEventListener("DOMContentLoaded", function() {
-    const navbarTitle = document.querySelector(".navbar-title");
-    const navbarCollapse = document.querySelector(".collapse");
-
-    navbarTitle.addEventListener("click", function() {
-        navbarCollapse.classList.toggle("show");
-    });
-});
