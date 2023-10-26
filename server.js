dee822b0f39150e255ecc9e64aadcc0d304487ff
Judd Ruscon T. Curamen <ruscon2513@gmail.com>
2023-10-26 23:23:52 +0800

    Many revisions were made to index.html, which is now login.html; Refreshened the look of the navbar and the products.html; Added some minor additions to the server.js, and many revisions to the styles.css

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
 app.listen(port,() => {
     console.log(`Example app listening on port ${port}`);
 })
